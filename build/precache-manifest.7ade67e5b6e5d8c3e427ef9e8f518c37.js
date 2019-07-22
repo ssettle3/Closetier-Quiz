9e9dbcdc8f4d03314118c6809a16bb280efac871
Stephen Settle <ssettle3@gmail.com>
2019-07-21 20:42:11 -0400

    New build 7/21/19

---
+++ build/precache-manifest.7ade67e5b6e5d8c3e427ef9e8f518c37.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f378742e6f64c5bb73fe83625d756f",
+    "url": "/closetier-quiz/index.html"
+  },
+  {
+    "revision": "a24eab6051d2a410f4cf",
+    "url": "/closetier-quiz/static/css/main.990c3b13.chunk.css"
+  },
+  {
+    "revision": "f1387047357ddf579824",
+    "url": "/closetier-quiz/static/js/2.fe8cdbcc.chunk.js"
+  },
+  {
+    "revision": "a24eab6051d2a410f4cf",
+    "url": "/closetier-quiz/static/js/main.8a7ad798.chunk.js"
+  },
+  {
+    "revision": "6498ce6d951288952d10",
+    "url": "/closetier-quiz/static/js/runtime~main.647348a8.js"
+  },
+  {
+    "revision": "29001a0dd13e0dd26175139b29d609a9",
+    "url": "/closetier-quiz/static/media/background.29001a0d.png"
+  },
+  {
+    "revision": "8b5522eceb9a4b86fd071ed58d836b8e",
+    "url": "/closetier-quiz/static/media/brunch.8b5522ec.jpg"
+  },
+  {
+    "revision": "0282820ee8c00a791bf1519af80b6257",
+    "url": "/closetier-quiz/static/media/casual.0282820e.png"
+  },
+  {
+    "revision": "1ed1fdbb7e029df87eb2b90d78219382",
+    "url": "/closetier-quiz/static/media/datenight.1ed1fdbb.jpg"
+  },
+  {
+    "revision": "c82b56b4ca850d20c3ff60b11e72cfb7",
+    "url": "/closetier-quiz/static/media/day.c82b56b4.jpg"
+  },
+  {
+    "revision": "bbf899bf41ffc520c305cc50d3268686",
+    "url": "/closetier-quiz/static/media/ending.bbf899bf.jpg"
+  },
+  {
+    "revision": "3e1f74f8f9a6db81303a3a703a04a274",
+    "url": "/closetier-quiz/static/media/girlsnight.3e1f74f8.jpg"
+  },
+  {
+    "revision": "c139aba6b35fec662c219ef4a1898b37",
+    "url": "/closetier-quiz/static/media/hangingout.c139aba6.jpg"
+  },
+  {
+    "revision": "38c33047003f64361b36f8516d62b0af",
+    "url": "/closetier-quiz/static/media/play.38c33047.jpg"
+  },
+  {
+    "revision": "ae5b12b48eeddd93ed2740f5f3393f20",
+    "url": "/closetier-quiz/static/media/polished.ae5b12b4.png"
+  },
+  {
+    "revision": "406219fde1a9b8dcc46fcc672650a6ae",
+    "url": "/closetier-quiz/static/media/shopping.406219fd.jpg"
+  },
+  {
+    "revision": "687abb0c13cc67d695ac0a02e40d89ef",
+    "url": "/closetier-quiz/static/media/totallychill.687abb0c.jpg"
+  },
+  {
+    "revision": "022c33e9ad748102ecdb62e7aaf7432c",
+    "url": "/closetier-quiz/static/media/work.022c33e9.jpg"
+  }
+]);
